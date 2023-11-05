42d190eca4d30283eb11b89d91df2ad5f8f48331
aakankshatech <91897384+aakankshatech@users.noreply.github.com>
2023-11-05 13:29:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ParagraphWordCounter from './ParagraphWordCounter'
+
+function App() {
+  return (
+    <div className="App">
+      <ParagraphWordCounter/>
+    </div>
+  );
+}
+
+export default App;
